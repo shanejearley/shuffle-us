10564c44b88ed78195a253ab7c6d4ce13e6f70bf
shanejearley <shane.earley10@gmail.com>
2021-06-20 19:11:05 -0400

    Add all possible intents

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 const speech = require('@google-cloud/speech');
 const fs = require('fs');
 
-const client = new discord.Client({ ws: { intents: ['GUILD_VOICE_STATES'] } });
+const client = new discord.Client({ ws: { intents: ['GUILDS', 'GUILD_PRESENCES', 'GUILD_MEMBERS', 'GUILD_VOICE_STATES'] } });
 const speechClient = new speech.SpeechClient();
 
 const config = {
@@ -14,8 +14,6 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
   client.user.setActivity(`Serving ${client.guilds.cache.size} servers`);
 });
-
-client.on('debug', console.log);
 
 client.on("message", async message => {
   if(message.author.bot) return;
@@ -45,51 +43,52 @@ client.on("message", async message => {
       if (!speaking) return;
       
       console.log('Listening...')
+        
+      const audio = connection.receiver.createStream(user, { mode: 'pcm' });
 
-      try {
-        
-        const audio = connection.receiver.createStream(user, { mode: 'pcm' });
+      
+      const audioFileName = './recordings/' + user.id + '_' + Date.now() + '.pcm';
+      
+      audio.pipe(fs.createWriteStream(audioFileName));
+      console.log('Streaming...')
+      
+      audio.on('end', async () => {
 
-        
-        const audioFileName = './recordings/' + user.id + '_' + Date.now() + '.pcm';
-        
-        audio.pipe(fs.createWriteStream(audioFileName));
-        console.log('Streaming...')
-        
-        audio.on('end', async () => {
-          console.log('Processing...')
-          fs.stat(audioFileName, async (err, stat) => {
-            if (!err && stat.size) {
-              const file = fs.readFileSync(audioFileName);
-              const audioBytes = file.toString('base64');
-              const audio = {
-                content: audioBytes,
-              };
-              const config = {
-                encoding: 'LINEAR16',
-                sampleRateHertz: 48000,
-                languageCode: 'en-US',
-                audioChannelCount: 2,
-              };
-              const request = {
-                audio: audio,
-                config: config,
-              };
-              console.log('Transcribing...')
-              const [response] = await speechClient.recognize(request);
-              const transcription = response.results
-              .map(result => result.alternatives[0].transcript)
-              .join('\n');
-              console.log(`I think ${user.username} said ${transcription}`)
-              message.reply(transcription);
-            } else {
-              console.log(JSON.stringify(err))
-            }
-          });
+        console.log('Processing...')
+
+        fs.stat(audioFileName, async (err, stat) => {
+          if (!err && stat.size) {
+            const file = fs.readFileSync(audioFileName);
+            const audioBytes = file.toString('base64');
+            const audio = {
+              content: audioBytes,
+            };
+            const config = {
+              encoding: 'LINEAR16',
+              sampleRateHertz: 48000,
+              languageCode: 'en-US',
+              audioChannelCount: 2,
+            };
+            const request = {
+              audio: audio,
+              config: config,
+            };
+
+            console.log('Transcribing...')
+
+            const [response] = await speechClient.recognize(request);
+            const transcription = response.results
+            .map(result => result.alternatives[0].transcript)
+            .join('\n');
+
+            console.log(`I think ${user.username} said ${transcription}`)
+            
+            message.reply(transcription);
+          } else {
+            console.log(JSON.stringify(err))
+          }
         });
-      } catch (err) {
-        console.log(JSON.stringify(err))
-      }
+      });
 
     });
   }
