639006e121c1108e40bb0df065dc5c358a749c78
shanejearley <shane.earley10@gmail.com>
2021-06-20 22:01:21 -0400

    Switch to simple text bot

--- bot.js
+++ bot.js
@@ -1,9 +1,6 @@
 const discord = require("discord.js");
-const speech = require('@google-cloud/speech');
-const fs = require('fs');
 
 const client = new discord.Client();
-const speechClient = new speech.SpeechClient();
 
 const config = {
   token: process.env.BOT_TOKEN,
@@ -19,84 +16,24 @@ client.on("debug", console.log)
 
 client.on("message", async message => {
   try {
-    if(message.author.bot) return;
-    if(!message.content.startsWith(config.prefix)) return;
+    if (message.author.bot) return;
+    if (!message.content.startsWith(config.prefix)) return;
   
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
        
-    if(command === "ping") {
+    if (command === "ping") {
       const m = await message.channel.send("Ping?");
       m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
     }
   
-    if(command === "join") {
+    if (command === "shuffle-us") {
       if (!message.member.voice.channel) return message.reply('Please join a voice channel first!');
-      if ((message.member.voice.channel.members.filter((e) => client.user.id === e.user.id).size > 0)) return message.reply(`I'm already in your voice channel!`);
+
+      const { members } = message.member.voice.channel;
+      return message.reply(JSON.stringify(members));
       
-      if (!message.member.voice.channel.joinable) return message.reply(`I don't have permission to join that voice channel!`);
-      if (!message.member.voice.channel.speakable) return message.reply(`I don't have permission to speak in that voice channel!`);
-  
-      const connection = await message.member.voice.channel.join();
-      connection.play('ding.wav');
-  
-      connection.on('speaking', (user, speaking) => {
-  
-        console.log('Hearing...')
-  
-        if (!user) return;
-        if (user.bot) return;
-        if (!speaking) return;
-        
-        console.log('Listening...')
-          
-        const audio = connection.receiver.createStream(user, { mode: 'pcm' });
-  
-        
-        const audioFileName = './recordings/' + user.id + '_' + Date.now() + '.pcm';
-        
-        audio.pipe(fs.createWriteStream(audioFileName));
-        console.log('Streaming...')
-        
-        audio.on('end', async () => {
-  
-          console.log('Processing...')
-  
-          fs.stat(audioFileName, async (err, stat) => {
-            if (!err && stat.size) {
-              const file = fs.readFileSync(audioFileName);
-              const audioBytes = file.toString('base64');
-              const audio = {
-                content: audioBytes,
-              };
-              const config = {
-                encoding: 'LINEAR16',
-                sampleRateHertz: 48000,
-                languageCode: 'en-US',
-                audioChannelCount: 2,
-              };
-              const request = {
-                audio: audio,
-                config: config,
-              };
-  
-              console.log('Transcribing...')
-  
-              const [response] = await speechClient.recognize(request);
-              const transcription = response.results
-              .map(result => result.alternatives[0].transcript)
-              .join('\n');
-  
-              console.log(`I think ${user.username} said ${transcription}`)
-  
-              message.reply(transcription);
-            } else {
-              console.log(JSON.stringify(err))
-            }
-          });
-        });
-  
-      });
+
     }
   } catch (err) {
     console.log(JSON.stringify(err))
