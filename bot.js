bbb2c11a9fda23fcd9f21e9912d0f6d2eb8e35a8
shanejearley <shane.earley10@gmail.com>
2021-06-20 23:19:20 -0400

    Add copy of array to shuffle fn

--- bot.js
+++ bot.js
@@ -54,7 +54,7 @@ client.on("message", async message => {
 });
 
 const shuffleArray = (array) => {
-  const shuffledArray = [];
+  const shuffledArray = array;
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [shuffledArray[i], shuffledArray[j]] = [array[j], array[i]];
